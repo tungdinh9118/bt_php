051ddacac2a0e4c9dd8ed462cc7330c23f3d3404
tungdinh9118 <30312405+tungdinh9118@users.noreply.github.com>
2017-07-29 18:17:44 +0700

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,14 +1,19 @@
 <?php 
+	session_start();
+	$total = 0;
 	echo "Người mua:" . $_SESSION["name"] . "<br>";
 	echo  "Số điên thoai:" . $_SESSION["numberPhone"] ." <br>";
-	echo $"ngày sinh:" . $_SESSION["birthday"] "<br>";
+	echo "ngày sinh:" . $_SESSION["birthday"] . "<br>";
 	if ( empty($_SESSION["sanpham"])) {
-		echo "không có sản phẩm nào được mua";
+		echo "không có sản phẩm nào được mua" . "<br>";
 	}else{
 		foreach ($_SESSION["sanpham"] as $key => $value) {
-			echo "sanpham" . $key . "Giá" . $value.
-			$value += $value;
-			
+			echo "sanpham" . $key . "Giá" . $value . "<br>";
+			$total = $value;
+			$total += $value;
 		}
+		
+		
 	}
+	echo "tông tiền:" . $total;
 ?>
